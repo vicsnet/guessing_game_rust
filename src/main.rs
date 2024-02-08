27a238ea0cfc64d0cb79c6a29b4909d1031f97ea
vicsnet <adesanmiadebisi@gmail.com>
2024-02-08 23:52:11 +0100

    handling error

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,31 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess");
+    loop{
 
-    let mut guess = String::new();
+        println!("Please input your guess");
+    
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+            // let guess: u32 = guess.trim().parse().expect("Please type a number");
+            let guess: u32 = match guess.trim().parse(){
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+    
+        println!("You guessed: {guess}");
+    
+        match guess.cmp(&secret_number){
+            Ordering::Less =>println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => { 
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-        let guess: u32 = guess.trim().parse().expect("Please type a number");
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number){
-        Ordering::Less =>println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win"),
+                println!("You win");
+                break;
+            }
+        }
     }
 }
